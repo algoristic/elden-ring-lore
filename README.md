4942a6e4ec38134cb5e1186931367686ef95085a
Marco Herzig <male233@001NTB1066.ad.aconso.com>
2022-04-07 12:41:38 +0200

    Refactoring: mit `dewey` bauen

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # ELDEN RING Hintergrundgeschichte
-
 In diesem Projekt protokolliere ich Informationen, die ich im Laufe des Spielens von _ELDEN RING_ über dessen Hintergrundgeschichte erfahre. Ich versuche diese Informationen in einen Zusammenhang zu bringen, da dieser ohne eine konsequente Verfolgung nur schwer ersichtlich wird.
 
-[Link zum Protokoll](./Protokoll.adoc)
+## Online
+http://elden-ring.algoristic.de
+Gebaut mit [dewey](https://github.com/algoristic/dewey)
---
+++ build.bat
@@ -0,0 +1,19 @@
+@echo off
+
+set DEWEY=C:\dev\projects\misc\dewey
+
+set COMMAND=%DEWEY%\build.ps1
+set COMMAND=%COMMAND% -Src .
+set COMMAND=%COMMAND% -Dest .\build
+set COMMAND=%COMMAND% -Style %DEWEY%\src\main\resources\lib\style.css
+set COMMAND=%COMMAND% -StyleExtension %DEWEY%\\src\main\resources\web\custom.css
+set COMMAND=%COMMAND% -StyleTheme %DEWEY%\src\main\resources\web\themes\oxford-blue.css
+set COMMAND=%COMMAND% -Templates %DEWEY%\src\main\resources\templates
+set COMMAND=%COMMAND% -TocLevels 4
+set COMMAND=%COMMAND% -Production:$true
+set COMMAND=%COMMAND% -Flatten:$true
+set COMMAND=%COMMAND% -LogLevel INFO
+
+powershell.exe -ExecutionPolicy Unrestricted -Command %COMMAND%
+
+pause
